f1b332092fa74a17b34600ce388c60d88c55c8eb
Johannes Grohmann <jo102tz@users.noreply.github.com>
2020-04-07 12:42:26 +0200

    Update RESTClient.java

--- utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/util/RESTClient.java
+++ utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/util/RESTClient.java
@@ -23,6 +23,9 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.UriBuilder;
 
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.glassfish.jersey.apache.connector.ApacheClientProperties;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.grizzly.connector.GrizzlyConnectorProvider;
@@ -38,8 +41,8 @@ public class RESTClient<T> {
 	 * Default and max size for connection pools. We estimate a good size by using the available processor count.
 	 */
 
-//	private static final int DEFAULT_POOL_SIZE = 500;
-//	private static final int MAX_POOL_SIZE = 100000;
+	private static final int DEFAULT_POOL_SIZE = 500;
+	private static final int MAX_POOL_SIZE = 100000;
 	
 	private static final int DEFAULT_CONNECT_TIMEOUT = 400;
 	private static final int DEFAULT_READ_TIMEOUT = 3000;
@@ -82,11 +85,12 @@ public class RESTClient<T> {
 		ClientConfig config = new ClientConfig();
 		config.property(ClientProperties.CONNECT_TIMEOUT, connectTimeout);
 		config.property(ClientProperties.READ_TIMEOUT, readTimeout);
-		//PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-	    //connectionManager.setMaxTotal(MAX_POOL_SIZE);
-	    //connectionManager.setDefaultMaxPerRoute(DEFAULT_POOL_SIZE);
-	    //config.property(ApacheClientProperties.CONNECTION_MANAGER, connectionManager);
-		config.connectorProvider(new GrizzlyConnectorProvider());
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+	    connectionManager.setMaxTotal(MAX_POOL_SIZE);
+	    connectionManager.setDefaultMaxPerRoute(DEFAULT_POOL_SIZE);
+	    config.property(ApacheClientProperties.CONNECTION_MANAGER, connectionManager);
+		config.connectorProvider(new ApacheConnectorProvider());
+		//config.connectorProvider(new GrizzlyConnectorProvider());
 		client = ClientBuilder.newClient(config);
 		service = client.target(UriBuilder.fromUri(hostURL).build());
 		applicationURI = application;
